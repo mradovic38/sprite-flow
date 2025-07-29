b332f9ce166131da8514597e7307cc295c8aa6c2
mradovic38 <mihailo.radovic11@gmail.com>
2025-07-29 16:02:29 +0200

    Add conditional vector field abstract class

---
+++ training/conditional_vector_field.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod, ABC
+
+import torch
+from torch import nn
+
+class ConditionalVectorField(nn.Module, ABC):
+    """
+    MLP-parametrization of the learned vector field u_t^theta(x)
+    """
+
+    @abstractmethod
+    def forward(self, x: torch.Tensor, t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the conditional vector field.
+        :param x: shape (bs, c, h, w)
+        :param t: shape (bs, 1, 1, 1)
+        :param y: shape (bs,)
+        :return: u_t^theta(x|y)
+        """
+        pass
