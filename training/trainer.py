44f5de43f34ecc8f8d7aee9bd1bff82cf499729a
mradovic38 <mihailo.radovic11@gmail.com>
2025-08-01 14:43:14 +0200

    Update trainer code structure

--- training/trainer.py
+++ training/trainer.py
@@ -12,9 +12,10 @@ from lr_scheduling import CosineWarmupScheduler
 
 
 class Trainer(ABC):
-    def __init__(self, model: nn.Module) -> None:
+    def __init__(self, model: nn.Module, checkpoint_path: str = "model_checkpoint.pt") -> None:
         super().__init__()
         self.model = model
+        self.checkpoint_path = checkpoint_path
 
     @abstractmethod
     def get_train_loss(self, **kwargs) -> torch.Tensor:
@@ -33,7 +34,6 @@ class Trainer(ABC):
             device: torch.device,
             lr: float = 1e-3,
             validate_every: int = 1,
-            checkpoint_path: str = "model_checkpoint.pt",
             resume: bool = False,
             lr_warmup_steps_frac: float = 0.1,
             **kwargs
@@ -44,7 +44,6 @@ class Trainer(ABC):
         :param device: device to train on
         :param lr: learning rate
         :param validate_every: validation frequency
-        :param checkpoint_path: path to checkpoint file
         :param resume: whether to resume training or to start over, overwriting the checkpoint file
         :param lr_warmup_steps_frac: learning rate warmup steps - fraction of the total training steps
         """
@@ -61,8 +60,8 @@ class Trainer(ABC):
         best_val_loss = float("inf")
 
         # Optionally load from checkpoints
-        if resume and os.path.exists(checkpoint_path):
-            checkpoint = torch.load(checkpoint_path, map_location=device)
+        if resume and os.path.exists(self.checkpoint_path):
+            checkpoint = torch.load(self.checkpoint_path, map_location=device)
             self.model.load_state_dict(checkpoint["model_state"])
             opt.load_state_dict(checkpoint["optimizer_state"])
             best_val_loss = checkpoint["best_val_loss"]
@@ -98,7 +97,7 @@ class Trainer(ABC):
                         "model_state": self.model.state_dict(),
                         "optimizer_state": opt.state_dict(),
                         "best_val_loss": best_val_loss,
-                    }, checkpoint_path)
+                    }, self.checkpoint_path)
                     log["checkpoint"] = "saved"
 
                 self.model.train()
@@ -110,15 +109,26 @@ class Trainer(ABC):
 
 
 class UnguidedTrainer(Trainer):
-    def __init__(self, path: GaussianConditionalProbabilityPath, model: nn.Module) -> None:
-        super().__init__(model)
+    def __init__(
+            self,
+            path: GaussianConditionalProbabilityPath,
+            model: nn.Module,
+            experiment_name: str = 'unet',
+            num_images_for_valid: int = 5,
+    ) -> None:
+        super().__init__(model, os.path.join(experiment_name, 'checkpoint.pt'))
         self.path = path
+        self.experiment_name = experiment_name
+        self.num_images_for_valid = num_images_for_valid
 
     def get_train_loss(self, batch_size: int) -> torch.Tensor:
         return self._compute_loss(batch_size, mode='train')
 
     def get_validation_loss(self, batch_size: int) -> torch.Tensor:
         return self._compute_loss(batch_size, mode='val')
+
+    def save_validation_images(self, images: torch.Tensor) -> None:
+        raise NotImplementedError() # TODO: implement
 
     def _compute_loss(self, batch_size: int, mode: str = 'train') -> torch.Tensor:
         # Sample from p_data
@@ -131,5 +141,7 @@ class UnguidedTrainer(Trainer):
 
         # Regress and output loss
         ut_theta = self.model(x, t)  # (batch_size, 4, 128, 128)
+        if mode == 'val' and ut_theta.shape[0] <= self.num_images_for_valid:
+            self.save_validation_images(ut_theta[:self.num_images_for_valid])
         ut_ref = self.path.conditional_vector_field(x, z, t)  # (batch_size, 4, 128, 128)
         return torch.mean(torch.sum(torch.square(ut_theta - ut_ref), dim=-1))
