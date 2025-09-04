064a827193179d3c0b14830be48711919a772976
mradovic38 <mihailo.radovic11@gmail.com>
2025-09-04 12:46:08 +0200

    Fix save_generated_assets function. Upload assets.

--- utils/helpers.py
+++ utils/helpers.py
@@ -101,7 +101,7 @@ def save_generated_assets(images: List[Image.Image], num_timesteps: int, path: s
     j = 0
     for i, img in enumerate(images):
         while True:
-            if not glob.glob(f"{path}/image_{i + j}"):
+            if not glob.glob(f"{path}/image_{i + j}" + "*"):
                 img.save(f"assets/unet/image_{i + j}-{num_timesteps}.png")
                 break
             j += 1
